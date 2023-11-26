66a91eb19ad6516a7cf5c466ea73c87d3715bbc6
Nábělek Jiří <jiri.nabelek@skodagroup.com>
2023-11-26 23:09:44 +0100

    Imports removed

--- buffer.py
+++ buffer.py
@@ -1,7 +1,4 @@
 from processing import ParquetProcess
-import pandas as pd
-import numpy as np
-import os
 
 class ParquetData:
   def __init__(self):
@@ -23,15 +20,3 @@ class ParquetData:
             self.data[sign_name] = {}
           self.data[sign_name][participant_folder] = ParquetProcess(file_path, landmark_id, max_length)
     print("Data read completed!")
-
-
-path = "C:/Skoda_Digital/Materials/Documents_FJFI/SU2/asl-signs"
-selected_landmark_indices = [33, 133, 159, 263, 46, 70, 4, 454, 234, 10, 338, 297, 332, 61, 291, 0, 78, 14, 317,
-                             152, 155, 337, 299, 333, 69, 104, 68, 398]
-
-df_train = pd.read_csv(path + "/train_mod.csv", sep=",")
-df_train.head()
-
-data_load = ParquetData()
-data_load.read_all(path, df_train, selected_landmark_indices, 37)
-# data_load.data["milk"]["4024"].tensor
